8eaa87a5c3465459dcdbc8cfb4505d402df39316
Davinder Rai <Davinder.Rai@SHEAGlobal.com>
2022-02-22 12:18:38 -0800

    added field to it

--- 01_MyTable_create.sql
+++ 01_MyTable_create.sql
@@ -1,8 +1,14 @@
-
-
+if exists (
+  select * from sys.tables where name = N'myTable'
+)
+BEGIN
+    DROP TABLE myTable
+END 
+GO
 CREATE TABLE myTable
 (
     Id INT IDENTITY,
     CustomerCode NVARCHAR(50),
     SoldToAddress1 NVARCHAR(50),
+    SoldToAddress2 NVARCHAR(50),
 )

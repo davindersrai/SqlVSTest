4b84ffb7d1c5ec0ca70f4e1b034bd7e5f18cdad9
Davinder Rai <Davinder.Rai@SHEAGlobal.com>
2022-02-24 09:21:46 -0800

    added create database

--- 01_MyTable_create.sql
+++ 01_MyTable_create.sql
@@ -1,15 +1,17 @@
 /*
 adding comments here 
 */
-
+--CREATE DATABASE SQLVSTest
+ 
 if exists (
   select * from sys.tables where name = N'myTable'
 )
 BEGIN
     DROP TABLE myTable
 END 
-GO
-CREATE TABLE myTable
+
+
+CREATE TABLE  dbo.myTable
 (
     Id INT IDENTITY,
     CustomerCode NVARCHAR(50),
